35f7c9db7d59a19f2e2eaeb1e1e122874ed5c5f9
Siflorite <monokhrom@163.com>
2026-09-10 15:20:59 +0800

    feat(mdns): mdns cache natrual TTL expiration

--- components/mdns/private_include/mdns_private.h
+++ components/mdns/private_include/mdns_private.h
@@ -112,6 +112,8 @@
 
 #define MDNS_TIMER_PERIOD_US        (CONFIG_MDNS_TIMER_PERIOD_MS*1000)
 
+#define MDNS_US_PER_SEC             1000000LL
+
 #define queueToEnd(type, queue, item)       \
     if (!queue) {                           \
         queue = item;                       \
@@ -412,6 +414,20 @@ typedef enum {
 typedef uint8_t mdns_cache_record_mask_t;
 
 /**
+ * @brief   mDNS cache expiry structure
+ */
+typedef struct mdns_cache_expiry_s {
+    bool queued;
+    mdns_cache_record_mask_t record_mask;
+    struct mdns_cache_expiry_s *next;
+
+    struct mdns_cache_entry_s *entry;
+    struct mdns_service_cache_s *service;
+    struct mdns_cache_addr_s *addr;
+    int64_t expires_at_us;  /*!< record absolute expiration time in microseconds */
+} mdns_cache_expiry_t;
+
+/**
  * @brief   mDNS cache ADDR list structure
  */
 typedef struct mdns_cache_addr_s {
@@ -428,16 +444,16 @@ typedef struct mdns_service_cache_s {
     char *service;
     char *proto;
     // PTR
-    bool ptr_present;   /*!< true if PTR record is present */
+    bool ptr_present;                           /*!< true if PTR record is present */
     uint32_t ptr_ttl;
     // SRV
-    bool srv_present;   /*!< true if SRV record is present */
+    bool srv_present;                           /*!< true if SRV record is present */
     uint16_t priority;
     uint16_t weight;
     uint16_t port;
     uint32_t srv_ttl;
     // TXT
-    bool txt_present;   /*!< true if TXT record is present */
+    bool txt_present;                           /*!< true if TXT record is present */
     mdns_txt_linked_item_t *txt_list;
     uint32_t txt_ttl;
     // To-sync flags
